88d8cd9abc2fea3eaffd6eeea71884afbb1036b4
Zhiznevskiy_Yura <zhizhnevskiy@yandex.ru>
2020-12-04 18:09:35 +0300

    Update index.php

--- 2020.12.04/index.php
+++ 2020.12.04/index.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-    echo "Hello World";
+    echo "Hello World<br>";
+    echo 5 * 3 . "<br>";
+    echo 2**3 . "<br>";
     ?>
 </body>
+
 </html>
